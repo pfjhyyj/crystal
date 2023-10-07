847c934d4257733a7e3ff99d4c7eb71d5bb56774
chipunderblade <pfjhyyj@qq.com>
2023-10-07 16:01:23 +0800

    feat: add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, type UserConfig } from 'vite'
+import { join } from 'path'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ArcoResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default ({ mode }): UserConfig => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ArcoResolver()]
+      }),
+      Components({
+        resolvers: [
+          ArcoResolver({
+            sideEffect: true
+          })
+        ]
+      })
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+          secure: true
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@': join(__dirname, 'src')
+      }
+    }
+  })
+}
