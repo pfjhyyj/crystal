7e5d68daf52717e57c5034bd6056c85741c58a07
chipunderblade <pfjhyyj@qq.com>
2023-10-17 22:53:51 +0800

    fix: fix login

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,5 +1,37 @@
 import request from '@/utils/request'
 import { type RouteRecordNormalized } from 'vue-router'
+import { type UserState } from '@/store/modules/user/types.ts'
+
+export interface LoginData {
+  username: string
+  password: string
+}
+
+export interface LoginRes {
+  token: string
+}
+
+export async function login (data: LoginData) {
+  return await request<LoginRes>({
+    url: '/api/user/login',
+    method: 'post',
+    data
+  })
+}
+
+export async function logout () {
+  return await request<LoginRes>({
+    url: '/api/user/logout',
+    method: 'post'
+  })
+}
+
+export async function getUserInfo () {
+  return await request<UserState>({
+    url: '/api/user/info',
+    method: 'post'
+  })
+}
 
 export async function getMenuList () {
   return await request<RouteRecordNormalized[]>({
--- src/store/modules/user/index.ts
+++ src/store/modules/user/index.ts
@@ -1,14 +1,14 @@
-import { defineStore } from 'pinia';
+import { defineStore } from 'pinia'
 import {
   login as userLogin,
   logout as userLogout,
   getUserInfo,
-  LoginData,
-} from '@/api/user';
-import { setToken, clearToken } from '@/utils/auth';
-import { removeRouteListener } from '@/utils/route-listener';
-import { UserState } from './types';
-import useAppStore from '../app';
+  type LoginData
+} from '@/api/user'
+import { setToken, clearToken } from '@/utils/auth'
+import { removeRouteListener } from '@/utils/route-listener'
+import { type UserState } from './types'
+import useAppStore from '../app'
 
 const useUserStore = defineStore('user', {
   state: (): UserState => ({
@@ -27,65 +27,65 @@ const useUserStore = defineStore('user', {
     registrationDate: undefined,
     accountId: undefined,
     certification: undefined,
-    role: '',
+    role: ''
   }),
 
   getters: {
-    userInfo(state: UserState): UserState {
-      return { ...state };
-    },
+    userInfo (state: UserState): UserState {
+      return { ...state }
+    }
   },
 
   actions: {
-    switchRoles() {
-      return new Promise((resolve) => {
-        this.role = this.role === 'user' ? 'admin' : 'user';
-        resolve(this.role);
-      });
+    async switchRoles () {
+      return await new Promise((resolve) => {
+        this.role = this.role === 'user' ? 'admin' : 'user'
+        resolve(this.role)
+      })
     },
     // Set user's information
-    setInfo(partial: Partial<UserState>) {
-      this.$patch(partial);
+    setInfo (partial: Partial<UserState>) {
+      this.$patch(partial)
     },
 
     // Reset user's information
-    resetInfo() {
-      this.$reset();
+    resetInfo () {
+      this.$reset()
     },
 
     // Get user's information
-    async info() {
-      const res = await getUserInfo();
+    async info () {
+      const res = await getUserInfo()
 
-      this.setInfo(res.data);
+      this.setInfo(res.data)
     },
 
     // Login
-    async login(loginForm: LoginData) {
+    async login (loginForm: LoginData) {
       try {
-        const res = await userLogin(loginForm);
-        setToken(res.data.token);
+        const res = await userLogin(loginForm)
+        setToken(res.data.token)
       } catch (err) {
-        clearToken();
-        throw err;
+        clearToken()
+        throw err
       }
     },
-    logoutCallBack() {
-      const appStore = useAppStore();
-      this.resetInfo();
-      clearToken();
-      removeRouteListener();
-      appStore.clearServerMenu();
+    logoutCallBack () {
+      const appStore = useAppStore()
+      this.resetInfo()
+      clearToken()
+      removeRouteListener()
+      appStore.clearServerMenu()
     },
     // Logout
-    async logout() {
+    async logout () {
       try {
-        await userLogout();
+        await userLogout()
       } finally {
-        this.logoutCallBack();
+        this.logoutCallBack()
       }
-    },
-  },
-});
+    }
+  }
+})
 
-export default useUserStore;
+export default useUserStore
